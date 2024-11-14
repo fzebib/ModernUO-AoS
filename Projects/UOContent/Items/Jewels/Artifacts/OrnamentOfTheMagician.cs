3cce592f9164fa3eff0d4d474cac3d4ef39a4f56
Firas Zebib <firas.zebib@bain.com>
2024-11-14 17:48:59 -0500

    attempt curse on orny

--- Projects/UOContent/Items/Jewels/Artifacts/OrnamentOfTheMagician.cs
+++ Projects/UOContent/Items/Jewels/Artifacts/OrnamentOfTheMagician.cs
@@ -1,21 +1,23 @@
 using ModernUO.Serialization;
+using Server.Items;
 
-namespace Server.Items;
-
-[SerializationGenerator(0, false)]
-public partial class OrnamentOfTheMagician : GoldBracelet
+namespace Server.Items
 {
-    [Constructible]
-    public OrnamentOfTheMagician()
+    [SerializationGenerator(0, false)]
+    public partial class OrnamentOfTheMagician : CursedArtifact
     {
-        Hue = 0x554;
-        Attributes.CastRecovery = 3;
-        Attributes.CastSpeed = 2;
-        Attributes.LowerManaCost = 10;
-        Attributes.LowerRegCost = 20;
-        Resistances.Energy = 15;
-    }
+        [Constructible]
+        public OrnamentOfTheMagician() : base(0x1088) // Item ID for a bracelet
+        {
+            Hue = 0x554; // Color of the ornament
+            Attributes.CastRecovery = 3;
+            Attributes.CastSpeed = 2;
+            Attributes.LowerManaCost = 10;
+            Attributes.LowerRegCost = 20;
+            Resistances.Energy = 15;
+        }
 
-    public override int LabelNumber => 1061105; // Ornament of the Magician
-    public override int ArtifactRarity => 11;
+        public override int LabelNumber => 1061105; // Ornament of the Magician
+        public override int ArtifactRarity => 11;
+    }
 }
