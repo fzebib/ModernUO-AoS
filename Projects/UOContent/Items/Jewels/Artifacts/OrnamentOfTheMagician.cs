faf9f9f297a5b8c9889718d5f381f4b8e690c9bf
Firas Zebib <firas.zebib@bain.com>
2024-11-14 17:59:12 -0500

    revert

--- Projects/UOContent/Items/Jewels/Artifacts/OrnamentOfTheMagician.cs
+++ Projects/UOContent/Items/Jewels/Artifacts/OrnamentOfTheMagician.cs
@@ -1,25 +1,21 @@
 using ModernUO.Serialization;
-using Server.Items;
 
-namespace Server.Items
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class OrnamentOfTheMagician : GoldBracelet
 {
-    [SerializationGenerator(0, false)]
-    public partial class OrnamentOfTheMagician : CursedArtifact
+    [Constructible]
+    public OrnamentOfTheMagician()
     {
-        [Constructible]
-        public OrnamentOfTheMagician() : base(0x1088) // Item ID for a bracelet
-        {
-            Hue = 0x554; // Color of the ornament
-            Attributes.CastRecovery = 3;
-            Attributes.CastSpeed = 2;
-            Attributes.LowerManaCost = 10;
-            Attributes.LowerRegCost = 20;
-            Resistances.Energy = 15;
-        }
-
-        public override int LabelNumber => 1061105; // Ornament of the Magician
-        
-        // Remove 'override' and declare as a regular property
-        public int ArtifactRarity => 11;
+        Hue = 0x554;
+        Attributes.CastRecovery = 3;
+        Attributes.CastSpeed = 2;
+        Attributes.LowerManaCost = 10;
+        Attributes.LowerRegCost = 20;
+        Resistances.Energy = 15;
     }
+
+    public override int LabelNumber => 1061105; // Ornament of the Magician
+    public override int ArtifactRarity => 11;
 }
