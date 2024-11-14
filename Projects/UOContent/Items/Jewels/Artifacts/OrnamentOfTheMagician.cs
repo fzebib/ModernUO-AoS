626b760dec432e7fc0b7dcf79ded32a4a42c183d
Firas Zebib <firas.zebib@bain.com>
2024-11-14 18:05:08 -0500

    try again for orny curse

--- Projects/UOContent/Items/Jewels/Artifacts/OrnamentOfTheMagician.cs
+++ Projects/UOContent/Items/Jewels/Artifacts/OrnamentOfTheMagician.cs
@@ -1,21 +1,53 @@
 using ModernUO.Serialization;
+using Server.Items;
+using Server.Mobiles;
 
-namespace Server.Items;
-
-[SerializationGenerator(0, false)]
-public partial class OrnamentOfTheMagician : GoldBracelet
+namespace Server.Items
 {
-    [Constructible]
-    public OrnamentOfTheMagician()
+    [SerializationGenerator(0, false)]
+    public partial class OrnamentOfTheMagician : BaseJewel, ICursedItem
     {
-        Hue = 0x554;
-        Attributes.CastRecovery = 3;
-        Attributes.CastSpeed = 2;
-        Attributes.LowerManaCost = 10;
-        Attributes.LowerRegCost = 20;
-        Resistances.Energy = 15;
-    }
+        private readonly CursedArtifactHelper _cursedHelper = new();
 
-    public override int LabelNumber => 1061105; // Ornament of the Magician
-    public override int ArtifactRarity => 11;
+        [Constructible]
+        public OrnamentOfTheMagician() : base(0x1088, Layer.Bracelet)
+        {
+            Hue = 0x554;
+            Attributes.CastRecovery = 3;
+            Attributes.CastSpeed = 2;
+            Attributes.LowerManaCost = 10;
+            Attributes.LowerRegCost = 20;
+            Resistances.Energy = 15;
+        }
+
+        public override int LabelNumber => 1061105; // Ornament of the Magician
+        public int ArtifactRarity => 11;
+
+        // Implement IsCursed property via the helper
+        public bool IsCursed => _cursedHelper.IsCursed;
+
+        // Apply the cursed effects when equipped or picked up
+        public override bool OnEquip(Mobile from)
+        {
+            if (IsCursed)
+            {
+                _cursedHelper.ApplyCursedEffects(from);
+                _cursedHelper.StartCurseTimer(from, TimeSpan.FromMinutes(5));
+            }
+            return base.OnEquip(from);
+        }
+
+        public override void OnRemoved(object parent)
+        {
+            base.OnRemoved(parent);
+            if (parent is Mobile owner && IsCursed)
+            {
+                _cursedHelper.RemoveCursedEffects(owner);
+            }
+        }
+
+        // These methods are exposed from the helper to satisfy the ICursedItem interface
+        public void ApplyCursedEffects(Mobile owner) => _cursedHelper.ApplyCursedEffects(owner);
+        public void RemoveCursedEffects(Mobile owner) => _cursedHelper.RemoveCursedEffects(owner);
+    }
 }
