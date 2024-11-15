f6765ae192977f36e20c091489f10639fab6709a
Firas <fzebib@gmail.com>
2024-11-14 20:55:01 -0500

    artifact on boss corpse

--- Projects/UOContent/Mobiles/Special/BaseChampion.cs
+++ Projects/UOContent/Mobiles/Special/BaseChampion.cs
@@ -261,36 +261,11 @@ public abstract partial class BaseChampion : BaseCreature
             }
         }
 
-   // New Code: Attempt to generate an artifact and give to a random eligible player
+    // New Code: Attempt to generate an artifact
     var artifact = GetArtifact();
     if (artifact != null)
     {
-        // Ensure there's at least one eligible player
-        var rights = GetLootingRights(DamageEntries, HitsMax);
-        var eligiblePlayers = new List<Mobile>();
-
-        for (var i = rights.Count - 1; i >= 0; --i)
-        {
-            var ds = rights[i];
-
-            if (ds.m_HasRight)
-            {
-                eligiblePlayers.Add(ds.m_Mobile);
-            }
-        }
-
-        // If there are eligible players, give the artifact to one of them
-        if (eligiblePlayers.Count > 0)
-        {
-            var recipient = eligiblePlayers.RandomElement();
-            recipient.AddToBackpack(artifact);
-            recipient.SendMessage("You have received an artifact for your valor in battle!");
-        }
-        else
-        {
-            // If no eligible players (fallback), drop the artifact on the corpse
-            c.DropItem(artifact);
-        }
+        c.DropItem(artifact); 
     }
 
         base.OnDeath(c);
