bec298b1101e1653eb48c7cd06f1eda47c459eed
Firas <fzebib@gmail.com>
2024-11-14 20:38:05 -0500

    revert

--- Projects/UOContent/Items/Jewels/Artifacts/OrnamentOfTheMagician.cs
+++ Projects/UOContent/Items/Jewels/Artifacts/OrnamentOfTheMagician.cs
@@ -1,55 +1,21 @@
 using ModernUO.Serialization;
-using Server.Mobiles;
 
-namespace Server.Items
+namespace Server.Items;
+
+[SerializationGenerator(0, false)]
+public partial class OrnamentOfTheMagician : GoldBracelet
 {
-    [SerializationGenerator(0, false)]
-    public partial class OrnamentOfTheMagician : GoldBracelet, ICursedItem
+    [Constructible]
+    public OrnamentOfTheMagician()
     {
-        private readonly CursedItemHelper _curseHelper;
-
-        [Constructible]
-        public OrnamentOfTheMagician()
-        {
-            Hue = 0x554;
-            Attributes.CastRecovery = 3;
-            Attributes.CastSpeed = 2;
-            Attributes.LowerManaCost = 10;
-            Attributes.LowerRegCost = 20;
-            Resistances.Energy = 15;
-
-            _curseHelper = new CursedItemHelper();
-        }
-
-        public override int LabelNumber => 1061105; // Ornament of the Magician
-        public override int ArtifactRarity => 11;
-
-        // ICursedItem implementation
-        public bool IsCursed => _curseHelper.IsCursed;
-
-        public void ApplyCursedEffects(Mobile owner)
-        {
-            _curseHelper.ApplyCursedEffects(owner);
-        }
-
-        public void RemoveCursedEffects(Mobile owner)
-        {
-            _curseHelper.RemoveCursedEffects(owner);
-        }
-
-        // This method is called whenever the item's parent changes (e.g., it’s added to a player’s backpack)
-        public override void OnParentChanged(object oldParent)
-        {
-            base.OnParentChanged(oldParent);
-
-            if (Parent is Mobile owner && IsCursed)
-            {
-                ApplyCursedEffects(owner);
-                _curseHelper.StartCurseTimer(owner);
-            }
-        }
-
-        // Prevent insurance while the item is cursed
-        public override bool CanBeInsured => !IsCursed;
+        Hue = 0x554;
+        Attributes.CastRecovery = 3;
+        Attributes.CastSpeed = 2;
+        Attributes.LowerManaCost = 10;
+        Attributes.LowerRegCost = 20;
+        Resistances.Energy = 15;
     }
+
+    public override int LabelNumber => 1061105; // Ornament of the Magician
+    public override int ArtifactRarity => 11;
 }
